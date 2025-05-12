37adcb5ed2e3bc4ef699f586b9fff09ca6d52551
davidsantana1 <davidsantanar01@gmail.com>
2025-05-12 00:31:03 -0400

    Fix text size in cart on mobile

--- Client/src/features/cart/Cart.tsx
+++ Client/src/features/cart/Cart.tsx
@@ -41,10 +41,12 @@ function Cart() {
                     src={product.imgSrc}
                     className="mr-5 h-24 w-24 rounded-xl object-cover"
                   />
-                  <span>{product.name}</span>
+                  <span className="text-sm sm:text-base">{product.name}</span>
                 </Link>
                 <div className="flex items-center pr-5">
-                  <span className="mr-5 font-bold">${product.price}.00</span>
+                  <span className="mr-5 text-sm font-bold sm:text-base">
+                    ${product.price}.00
+                  </span>
                   <Counter />
                 </div>
               </div>
