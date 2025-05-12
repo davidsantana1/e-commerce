6caa574bb516d35f5fcfca1e54b2f5164a4f60c5
davidsantana1 <davidsantanar01@gmail.com>
2025-05-11 20:09:42 -0400

    Fix address and account page padding

--- Client/src/features/address/Address.tsx
+++ Client/src/features/address/Address.tsx
@@ -3,7 +3,7 @@ import AddressForm from "./AddressForm";
 
 function Address() {
   return (
-    <div className="flex w-full flex-col items-center p-6 pt-2">
+    <div className="flex w-full flex-col items-center p-6 pt-12 xl:pt-2">
       <Heading className="text-4xl" margin="lg" weight="bold">
         My Address
       </Heading>
